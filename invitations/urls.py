748a4bcfbbadc4e95104938bcbc6da70e316889c
Zander <zander.aligato25@gmail.com>
2025-12-02 12:51:41 +0800

    feat(invitations): added commented line at urls.py for front-ends work

--- invitations/urls.py
+++ invitations/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # Band Admin Views
     path('invite/', views.band_admin_invite_musicians, name='band_admin_invite'),
     path('send/', views.send_invitation, name='send_invitation'),
+    #For Treasure. Enable line below and change if needed.
+    #path('sent_invitations', views.band_sent_invitations, name='band_sent_invitations'),
     
     # Musician Views  
     path('received/', views.musician_received_invitations, name='musician_invitations'),
